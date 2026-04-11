51f34f1fe8a8356667d751d1bbe2d34517bb332b
johnli777 <yuzhe.john.li777@gmail.com>
2026-04-11 16:50:14 -0700

    Rename bu_net.py and simplify the basic Unet model

---
+++ models/bu_net.py
@@ -0,0 +1,17 @@
+# models/bu_net_multitask_working.py
+import torch.nn as nn
+import segmentation_models_pytorch as smp
+
+class BUNet(nn.Module):
+    def __init__(self, encoder_name='resnet34', in_channels=2, num_classes=4):
+        super().__init__()
+        self.unet = smp.Unet(
+            encoder_name=encoder_name,
+            encoder_weights='imagenet',
+            in_channels=in_channels,
+            classes=num_classes,
+            activation=None
+        )
+ 
+    def forward(self, x):
+        return self.unet(x)
